4c8b3494c65fd68c31849cb45778f8291f9ef543
Anna D <adosev95@gmail.com>
2022-05-31 08:28:26 -0400

    functions practice from weekend and other file changes

--- javascript/classNotes/functions.js
+++ javascript/classNotes/functions.js
@@ -52,21 +52,63 @@ function aboutMe2(me, city, subject) {
 function difference(num1, num2) {
     return num2-num1
 }
-console.log(difference(2, 10))
+//console.log(difference(2, 10))
 
 //Class problem 2
 function multiplies(num1, num2) {
     return num2*num1
 }
-console.log(multiplies(2, 10))
+//console.log(multiplies(2, 10))
 
 //Class problem 3
 function divides(num1, num2) {
     return num2/num1
 }
-console.log(divides(5, 10))
+//console.log(divides(5, 10))
 
 //equality operators
-console.log(1 == '1'); //true, bc a number is eq to a number
-console.log(1 === '1'); //false, bc a number is not eq to a string
+//console.log(1 == '1'); //true, bc a number is eq to a number
+//console.log(1 === '1'); //false, bc a number is not eq to a string
 // === is more strict than ==
+
+//Post class notes
+//1. Write a function that takes in a billAmount, a tipPercentage, and returns the tip amount.
+function tipAmmount(bill, percentage) {
+    return bill*percentage
+}
+
+//2. Write a function that takes in an array, and returns the length of the array.
+function lengthOfArray(arrayParameter) {
+    let array = [1, 2, 3, 4, 5, 6]
+    return array.length
+}
+
+//3. Write a function that takes in the names of 3 friends and returns an array of those friends’ names.
+function friendsNames(name1, name2, name3) {
+    return [name1, name2, name3]
+}
+
+//4. Write a function that takes in an object of userData and returns the sentence
+    //`The user's name is __, they are located in __, and they love the color __!`
+function aboutUser() {
+    let userData = {
+        name3: 'Anna',
+        location: 'Pensacola',
+        favoriteColor: 'Blue'
+    }
+    return `The user's name is ${userData.name3}, they are located in ${userData.location}, and they love the color ${userData.favoriteColor}!`
+}
+
+
+//5. Write a function that takes in an array of numbers and returns the number in the last index
+function arrayOfNumbers() {
+    let array2 = [2, 4, 6, 8]
+    return array2.pop()
+}
+
+
+console.log(tipAmmount(225,.20))
+console.log(lengthOfArray())
+console.log(friendsNames("Anna", "Alex", "Molley"))
+console.log(aboutUser())
+console.log(arrayOfNumbers())
