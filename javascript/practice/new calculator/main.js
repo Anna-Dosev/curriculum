278140b09c2f43842ef4d734d1221b1a21cde8e8
Anna D <adosev95@gmail.com>
2022-06-28 17:09:40 -0400

    add notes for new-calculator

--- javascript/practice/new calculator/main.js
+++ javascript/practice/new calculator/main.js
@@ -16,31 +16,47 @@ const signs = {
     divide: "/"
 }
 
+const clearData = function() {
+    displayNumbers = '';
+    hasOperator = false;
+    sign = null;
+    x = 0;
+    y = 0;
+}
+
 const renderDisplay = function() {
     display.innerText = displayNumbers
 };
 
 numbers.addEventListener('click', function(e) { //have to put something in function parameter in order to access the event
-    const digit = e.target.innerText * 1; //forces the digit to be a number, bc by default its a string
-    displayNumbers += digit; //whatever the displayed number is, add digit ()
-    
-    if (!hasOperator) {
-        x += digit * 1;
-    } else {
-        y += digit * 1;
-    };
-    
     const isNumber = e.target.getAttribute('data-number') //passing in the name of the attribute from the html file
-        if (isNumber) {
+    const isDecimal = e.target.hasAttribute('data-decimal')
+    const isClear = e.target.hasAttribute('data-clear')
+    
+    if (isNumber) {
+        const digit = e.target.innerText; //forces the digit to be a number, bc by default its a string
+        displayNumbers += digit; //whatever the displayed number is, add digit ()
             //display.innerText = displayNumbers;
-            renderDisplay();
+            if (!hasOperator) {
+                x += digit * 1;
+            } else {
+                y += digit * 1;
+            };
+        renderDisplay();
+        } else if (isDecimal) { //decimal
+        const digit = e.target.innerText; 
+        displayNumbers += digit; 
+        renderDisplay();
+        } else if (isClear) { //clearing 'C'
+        clearData();
+        renderDisplay();
         }
-});
+    });
 
 operators.addEventListener('click', function(e) {
     hasOperator = true;
     const operator = e.target.getAttribute('data-operator');
-    sign = operator 
+    sign = operator; 
     if (!x) {
         x = displayNumbers * 1 //fristDigits = parseInt(displayNumbers); casting, forces string to be a number
     } 
@@ -53,13 +69,14 @@ operators.addEventListener('click', function(e) {
 equals.addEventListener('click', function() {
     let result = null; 
     if (signs[sign] === '+'){
-        result = x + y
+        result = x + y;
     } else if (signs[sign] === '-') {
-        result = x - y
+        result = x - y;
     } else if (signs[sign] === '*') {
-        result = x * y  
+        result = x * y;
     } else if (signs[sign] === '/') {
-        result = x / y
+        result = x / y;
     }
     display.innerText = result;
+    clearData();
 })
